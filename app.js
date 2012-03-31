a989c42f765a6735d185a2e4edc80c25bed654d1
Matt Swanson <mdswanson@sep.com>
2012-03-31 16:04:39 -0400

    add meta route at root

--- app.js
+++ app.js
@@ -5,18 +5,26 @@ var express = require('express')
     , inspect = require('util').inspect;
 
 app = express.createServer();
-app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
 
 var port = process.env.PORT || 8080;
 app.listen(port);
 
-app.get('/events', function(req, res) {
-    indyHackersUrl = 'https://www.google.com/calendar/feeds/ig7e0j6v8ub9q6kga256n77048@group.calendar.google.com/public/full?futureevents=true&singleevents=true&alt=jsonc&fields=items(title,location,when)&orderby=starttime&sortorder=ascending&max-results=10';
+app.get('/', function(req, res) {
+    var url = "https://www.google.com/calendar/feeds/ig7e0j6v8ub9q6kga256n77048@group.calendar.google.com/public/full?alt=jsonc&max-results=0";
     
-    rest.get(indyHackersUrl).on('complete', function(d) {
-        next_three = _.first(d.data.items, 5)
-        cleaned_data = _.map(next_three, clean_calendar);
+    rest.get(url).on('complete', function(r) {
+        cleaned_data = clean_meta(r);
+
+        res.json(cleaned_data);
+    });
+});
+
+app.get('/events', function(req, res) {
+    var url = 'https://www.google.com/calendar/feeds/ig7e0j6v8ub9q6kga256n77048@group.calendar.google.com/public/full?futureevents=true&singleevents=true&alt=jsonc&fields=items(title,location,when,details)&orderby=starttime&sortorder=ascending&max-results=10';
+    
+    rest.get(url).on('complete', function(d) {
+        cleaned_data = _.map(d.data.items, clean_calendar);
 
         //google api likes to not sort correctly
         cleaned_data = _.sortBy(cleaned_data, function(item){ return item.when; });
@@ -24,12 +32,27 @@ app.get('/events', function(req, res) {
     });
 });
 
+clean_meta = function(response) {
+    return {
+        'title': response['data']['title'],
+        'what': response['data']['details'],
+        'updated': new Date(response['data']['updated']).getTime() / 1000,
+        'routes': get_routes(),
+    }
+};
+
+get_routes = function() {
+    var routes = app.routes.routes['get'];
+    return _.pluck(routes, 'path')
+}
+
 clean_calendar = function(item) {
     return {
         'what': item['title'],
         'where': item['location'],
         'when': new Date(item['when'][0]['start']).getTime() / 1000,
+        'description': item['details'],
     }
-}
+};
 
 console.log('Server started on port %s', app.address().port);
